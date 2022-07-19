141700b8cc954a5cb601e84d7ecc228ca9ad98af
cuongdt <thecuong4298@gmail.com>
2022-07-20 00:45:28 +0700

    add condition show swagger

--- src/main/java/com/ngs/gateway/config/ApplicationConfig.java
+++ src/main/java/com/ngs/gateway/config/ApplicationConfig.java
@@ -1,14 +1,11 @@
 package com.ngs.gateway.config;
 
-import io.netty.handler.logging.LogLevel;
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class ApplicationConfig {
@@ -20,12 +17,8 @@ public class ApplicationConfig {
                 .getRouteDefinitions().collectList().block()
                 .stream()
                 .map(RouteDefinition::getId)
-                .filter(id -> !"openapi".equalsIgnoreCase(id))
+                .filter(id -> id.matches(".*-swagger"))
+                .map(id -> id.replace("-swagger", ""))
                 .forEach(swaggerUiParameters::addGroup);
-    }
-
-    @Bean
-    HttpClient httpClient() {
-        return HttpClient.create().wiretap("LoggingFilter", LogLevel.INFO, AdvancedByteBufFormat.TEXTUAL);
     }
 }
